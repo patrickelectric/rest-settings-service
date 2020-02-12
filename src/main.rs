7fffc1fa0565e9bac92422d53b80abe9213df2c9
Patrick José Pereira <patrickelectric@gmail.com>
2020-02-11 21:44:39 -0300

    wip
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,5 @@
-use std::fs::File;
-use std::io::prelude::*;
-
-use toml;
-
 mod settings;
 
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize, Debug, Default)]
-pub struct Receita {
-    quantidade: u8,
-    sal: u8,
-    oregano: bool,
-}
-
-#[derive(Serialize, Deserialize, Debug, Default)]
-pub struct Batata {
-    tipo_de_batata: String,
-    frita: bool,
-    receita: Receita,
-}
-
 fn main() -> std::io::Result<()> {
-    let file_name = "foo.json";
-
-    // Open if the file exist, otherwise create it
-    let mut file = File::open(file_name).unwrap_or_else(|_| {
-        File::create(file_name).unwrap_or_else(|error| {
-            panic!("{:#?}", error);
-        })
-    });
-
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
-    println!("> {}", contents);
-
-    let json_value = serde_json::to_value(Batata::default()).unwrap();
-
-    let mut setting = settings::Content::default();
-    setting.settings = Some(toml::Value::try_from(json_value).unwrap());
-    println!("{}", serde_json::to_string_pretty(&setting).unwrap());
-    println!("{}", toml::to_string(&setting).unwrap());
     Ok(())
 }
--- src/settings.rs
+++ src/settings.rs
@@ -6,6 +6,7 @@ use std::path::{Path, PathBuf};
 use chrono;
 use hex;
 use serde::{Deserialize, Serialize};
+use serde_json::json;
 use sha1::{Digest, Sha1};
 use toml;
 
@@ -27,14 +28,14 @@ pub struct Content {
 #[derive(Serialize, Deserialize, Debug)]
 pub struct SettingsManager {
     pub path: String,
-    pub settings: HashMap<String, Content>,
+    pub settings: serde_json::Map<String, serde_json::Value>,
 }
 
 impl Default for SettingsManager {
     fn default() -> Self {
         SettingsManager {
             path: format!("~/.config/{}", env!("CARGO_PKG_NAME")),
-            settings: HashMap::new(),
+            settings: serde_json::Map::new(),
         }
     }
 }
@@ -61,13 +62,22 @@ impl SettingsManager {
         Path::new(&self.path).join("default")
     }
 
+    /*
+    pub fn read_setting(&self, name: &str) -> Option<toml::Value> {
+        if !self.settings.contains_key(name) {
+            return None;
+        }
+
+        if !name.contains("/") {
+            return Some(self.settings[name]);
+        }
+        return self.settings;
+    }*/
+
     /// TODO: return something
     pub fn push(&mut self, mut content: Content) {
-        let item = self
-            .settings
-            .iter()
-            .find(|&(name, _content)| name == &content.header.name);
-        if item.is_some() {
+        //let item = &self.settings[&content.header.name];
+        if self.settings.contains_key(&content.header.name) {
             println!("Item already exist: {}", content.header.name);
             return;
         }
@@ -80,7 +90,9 @@ impl SettingsManager {
         }));
         content.header.hash = hex::encode(hasher.result());
 
-        self.settings.insert(content.header.name.clone(), content);
+        self.settings.insert(content.header.name.clone(), serde_json::to_value(&content).unwrap_or_else(|error| {
+            panic!("Failed to parse content to json: {:#?}\n{:#?}", error, content)
+        }));
     }
 
     /// Load all settings available in the manager path
@@ -100,31 +112,37 @@ impl SettingsManager {
             file.read_to_string(&mut contents)
                 .unwrap_or_else(|error| panic!("Failed to read settings file: {:#?}", error));
 
-            let toml_content: Content = toml::from_str(&contents)
-                .unwrap_or_else(|error| panic!("Failed to update settings vector: {:#?}", error));
-            self.settings
-                .insert(toml_content.header.name.clone(), toml_content);
+            if contents.is_empty() {
+                println!("File '{:#?}' is empty.", file);
+                return;
+            }
+
+            let content: Content = toml::from_str(&contents)
+                .unwrap_or_else(|error| panic!("Failed to update settings vector: {:#?}\n{:#?}\n{:#?}", error, &file, &contents));
+            self.settings.insert(content.header.name.clone(), serde_json::to_value(&content).unwrap_or_else(|error| {
+                panic!("Failed to parse content to json: {:#?}\n{:#?}", error, content)
+            }));
         }
     }
 
     /// Save all settings available in the manager path
     pub fn save(&self) {
-        for (name, setting) in &self.settings {
+        for (name, setting) in self.settings.clone() {
             // Create file name for the settings file
-            let mut file_name = Path::new(&self.path).join(name);
+            let mut file_name = Path::new(&self.path).join(&name);
             file_name.set_extension("toml");
 
             // Do the same thing for the default file
-            let mut default_file_name = self.get_default_folder().join(name);
+            let mut default_file_name = self.get_default_folder().join(&name);
             default_file_name.set_extension("toml");
 
             // Create settings file if it exist or not, we are going to rewrite the data
-            let mut file = File::create(file_name).unwrap_or_else(|error| {
-                panic!("Failed to create settings file: {:#?}", error);
+            let mut file = File::create(&file_name).unwrap_or_else(|error| {
+                panic!("Failed to create settings file: {:#?}\n{:#?}", error, file_name);
             });
 
             // Create the string that will be in the file
-            let toml_string = toml::to_string_pretty(&setting)
+            let toml_string = toml::to_string_pretty(&serde_json::from_value::<Content>(setting).unwrap())
                 .unwrap_or_else(|error| panic!("Failed to parse settings to toml: {:#?}", error));
 
             let _ = file.write_all(&toml_string.as_bytes());
@@ -162,7 +180,7 @@ mod tests {
 
     fn save() {
         let mut settings_manager = SettingsManager::new(Some(create_path()));
-        settings_manager.settings.clear();
+        settings_manager.settings = serde_json::Map::new();
 
         println!("Check settings path: {}", settings_manager.path);
         assert!(Path::new(&settings_manager.path).exists());
@@ -192,9 +210,11 @@ mod tests {
         settings_manager.push(content);
         settings_manager.save();
 
+        //&serde_json::from_value::<Content>(settings_manager.settings["test"]).unwrap()
+        let test_settings = serde_json::from_value::<Content>(settings_manager.settings["test"].clone()).unwrap();
         // Check file
         let content_toml_string =
-            toml::to_string_pretty(&settings_manager.settings["test"]).unwrap();
+            toml::to_string_pretty(&test_settings).unwrap();
 
         let mut file_name = Path::new(&settings_manager.path).join("test");
         file_name.set_extension("toml");
@@ -213,12 +233,12 @@ mod tests {
 
     fn load() {
         let settings_manager = SettingsManager::new(Some(create_path()));
-        let content = &settings_manager.settings["test"];
-        let settings = content.settings.as_ref().unwrap();
+        let settings = &settings_manager.settings["test"]["settings"];
+        //let settings = content.settings;
 
-        println!("Check test file contents..");
+        println!("Check test file contents.. {:#?}", settings);
         assert_eq!(settings["name"].as_str().unwrap(), "John Doe");
-        assert_eq!(settings["age"].as_integer().unwrap(), 43);
+        assert_eq!(settings["age"].as_i64().unwrap(), 43);
         assert_eq!(settings["address"]["city"].as_str().unwrap(), "London");
         assert_eq!(settings["phones"][1].as_str().unwrap(), "+44 2345678");
     }
